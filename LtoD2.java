f45e1f24588fbe1d353183903f91e2d48164eab0
EDUARDO DE JESUS MOREIRA <79948050+EduardoMoreira023@users.noreply.github.com>
2024-07-22 18:02:40 -0300

    New commits! (22/07)

---
+++ LtoD2.java
@@ -0,0 +1,10 @@
+// *** Esse programa não será compilado. ***
+class LtoD {
+public static void main(String args){
+long L;
+double D;
+D = 100123285.0;
+L = D; // Inválido <------- Não há conversão automática de double para long
+System.out.println("L and D: " + L + " " + D);
+}
+}
---
+++ SideEffects.java
@@ -0,0 +1,22 @@
+// Os efeitos colaterais podem ser importantes.
+
+class SideEffects {
+     public static void main (String args[]){
+        int i;
+
+        i = 0;
+
+/* Aqui, i é incrementada mesmo que a instrução if seja falsa. */
+
+	if(false & (++i < 100))
+	    System.out.println("this wont be displayed");
+	System.out.println("if statement executed: " + i);// exibe 1
+
+
+/* Nesse caso, i não é incrementada porque i operador de curto-circuito ignora o incremento. */
+
+	if (false && (++i < 100))
+	    System.out.println("this wont be displayed");
+	System.out.println("if statement executed: " + i);// continua exibindo 1 !!
+}
+}

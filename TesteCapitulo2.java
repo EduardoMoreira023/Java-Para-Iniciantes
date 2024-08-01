d19ad0ff59bf31eeb604bea3519489a9c06f6812
EDUARDO DE JESUS MOREIRA <79948050+EduardoMoreira023@users.noreply.github.com>
2024-08-01 18:28:41 -0300

    new commit

---
+++ TesteCapitulo2.java
@@ -0,0 +1,4 @@
+// Testes do Capítulo 2
+
+
+1. R= Os tipos de dados são importantes em Java pq essa é uma liguagem fortemente tipada, todas as operações têm a compatibilidade de seus tipos verificada pelo compilador. Operações inválidas não serão complidadas. Logo, a verificação minuciosa dos tipos ajuda a impedir a verificação cuidadosa dos tipos, todas as variáveis, expressões
